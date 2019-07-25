4fd41af6d9467c5c7c893864152abaef674d7e7a
RanyellHenrique <ranyellhenrique@gmail.com>
2019-07-25 16:45:24 -0300

    Projeto Final

--- src/aplication/Principal.java
+++ src/aplication/Principal.java
@@ -1,7 +1,13 @@
 package aplication;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
+
+import entities.Person;
+import entities.PessoaFisica;
+import entities.PessoaJuridica;
 
 public class Principal {
 
@@ -10,6 +16,8 @@ public class Principal {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
+		List<Person> list = new  ArrayList<>();
+		
 		System.out.print("Enter the number of tax payers: ");
 		int n = sc.nextInt();
 		
@@ -17,7 +25,28 @@ public class Principal {
 			System.out.println("Tax payer #"+ i +" data:");
 			System.out.print("or company (i/c)?");
 			char ch = sc.next().charAt(0);
+			System.out.print("Name: ");
+			sc.nextLine();
+			String name = sc.nextLine();
+			System.out.print("Anual income: ");
+			double rendaAnual = sc.nextDouble();
 			
+			if(ch == 'i') {
+				System.out.print("Health expenditures: ");
+				double gastosComSaude = sc.nextDouble();
+				list.add(new PessoaFisica(name, rendaAnual, gastosComSaude));
+			}else if (ch == 'c') {
+				System.out.print("Number of employees: ");
+				int numeroDeFuncionario = sc.nextInt();
+				list.add(new PessoaJuridica(name, rendaAnual, numeroDeFuncionario));
+			}
+		}
+		
+		System.out.println();
+		System.out.println("TAXES PAID: ");
+		
+		for(Person person : list) {
+			System.out.println(person.getName() +" $ " + person.tax());
 		}
 		
 		sc.close();
